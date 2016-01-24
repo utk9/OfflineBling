e5e48b03e1ee231769f3785d03842c1d360430aa
Charlie Zhang <peidong97@yahoo.ca>
2016-01-23 19:48:49 -0500

    get all data, and question/answer integration

--- php/test.php
+++ php/test.php
@@ -5,13 +5,35 @@
     header("content-type: text/xml");
     echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 
+    //twilio
     $twilioAPI = $config["twilioAPI"];
     $accountSID = $config["accountSID"];
     $authToken = $config["authToken"];
 
+    //wolfram api
     $wolframID = $config["wolframID"];
 
-    $message = $_POST["Body"];
+    //mysql
+    $servername = $config["servername"];
+    $serverusername = $config["username"];
+    $serverpassword = $config["password"];
+    $database = $config["database"];
+
+    function test_input($data) {
+      $data = trim($data);
+      $data = stripslashes($data);
+      $data = htmlspecialchars($data);
+      return $data;
+    }
+
+    function clean_string($string) {
+      $bad = array("SELECT","DELETE", "DROP", "FROM");
+      return str_replace($bad,"",$string);
+    }
+
+    $message = clean_string(test_input($_POST["Body"]));
+
+    //initialize vars
     $term = "";
     $plainText = "";
 	$searchSuccess = true; 
@@ -19,9 +41,10 @@
 
     //search type
     $messageSplit = explode(" ", $message);
-    if(strtolower($messageSplit[0]) == "wiki")
+    $searchType = strtolower($messageSplit[0]);
+    if($searchType == "wiki")
     {
-         //search user term
+        //search user term
         $xml=simplexml_load_file("https://en.wikipedia.org/w/api.php?action=query&format=xml&list=search&srsearch=" . substr(strstr($message, ' '), 1));
         $search=$xml->query->search;
         if(count($search->children()) == 0)
@@ -60,36 +83,72 @@
             $plainText = $xml->query->pages->page->extract;
             if(strlen($plainText) >= 800)
             {
-                $tooLong = true;
+                $plainText = substr($plainText, 0, 797) . "...";
             }
         }
     }
-    else if(strtolower($messageSplit[0]) == "wolfram")
+    else if($searchType == "wolfram")
     { 
         $xml = simplexml_load_file("http://api.wolframalpha.com/v2/query?input=" . substr(strstr($message, ' '), 1) . "&appid=" . $wolframID);
         foreach($xml->pod as $pod)
         {
-            $plainText .= $pod["title"] . ": " . $pod->subpod->plaintext . "\n";
-        } 
+            if($pod->subpod->plaintext != "")
+            {
+                $plainText .= htmlspecialchars($pod["title"] . ": " . $pod->subpod->plaintext . "\n");
+            }
+            else
+            {
+                $plainText .= htmlspecialchars($pod["title"]) . ": N/A\n"; 
+            }
+        }
         if(strlen($plainText) >= 800)
         {
-            $tooLong = true;
+            $plainText = substr($plainText, 0, 797) . "...";
         }
-        $plainText = htmlspecialchars($plainText);
-    }
+    } 
+    else if($searchType == "question")
+    {
+        // Create connection
+        $conn = new mysqli($servername, $serverusername, $serverpassword, $database);
+        $userQuestion = mysqli_real_escape_string($conn, substr(strstr($message, ' '), 1));
+        $query = "SELECT answer, count FROM Questions WHERE question='$userQuestion'";
+        $answerQuery = $conn->query($query);
 
-   
+        if($answerQuery->num_rows > 0) 
+        {
+            while($row = $answerQuery->fetch_assoc()) 
+            {
+                $plainText = $row["answer"];
+                if($plainText == "")
+                {
+                    $plainText == "Sorry, there is currently no submitted answer for this question. Please try again later.";
+                }
+                $currCount = $row["count"];
+                $currCount++;
+                $countQuery = "UPDATE Questions SET count='$currCount' WHERE question='$userQuestion'";
+                if ($conn->query($countQuery) !== TRUE) {
+                    error_log("couldn't update the count using countQuery");
+                }
+                break;
+            }
+        }
+        else
+        {
+            $plainText = "Well, that's a question we haven't heard yet. We'll submit it to the community to be answered.";
+            $newQuery = "INSERT INTO Questions (question, answer, count)
+    VALUES ('$userQuestion', '', '1')";
+            if($conn->query($newQuery) !== TRUE)
+            {
+                error_log("couldn't add new question using newQuery" . $conn->error);
+
+            }
+        }
+        
+    }
 
 ?>
 <Response>
 	<?php
-		if ($tooLong) 
-		{
-			echo "<Message>" . substr($plainText, 0, 797) . "...</Message>";
-		}
-		else
-		{
-		 	echo "<Message>" . $plainText . "</Message>"; 
-		}
+        echo "<Message>" . $plainText . "</Message>"; 
 	?>
 </Response>
